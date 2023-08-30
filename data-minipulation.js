6f11735cd48c9e7c72f53193a63a379435edd5cd
Win Tun <wintun.hlaing@outlook.com>
2023-08-30 20:29:05 +0630

    update

--- data-minipulation.js
+++ data-minipulation.js
@@ -4,3 +4,14 @@ const singleObject = user.reduce((i,j)=>({...i,...j}),{});
 console.log("Single Object Result>>",singleObject)
 const singleObjectSimpleMethod = Object.assign({},...user)
 console.log("Single Object Result>>",singleObjectSimpleMethod)
+
+
+/* Change to single object from an arry of having many object inside with key value parirs. */
+
+const keyValueUser=[
+{key: 'name', value: 'Win Tun'},
+{key: 'email', value: 'wintun1234@gmail.com'},
+{key: 'password', value: '234567usfakf'}
+]
+const result = keyValueUser.reduce((obj, item) => ({...obj, [item.key]: item.value}) ,{});
+console.log("Single Object Result>>",result)
