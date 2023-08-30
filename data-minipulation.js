e00096d857e53118371255d8c33874e048e25952
Win Tun <wintun.hlaing@outlook.com>
2023-08-30 19:54:08 +0630

    array to single object simple function

--- data-minipulation.js
+++ data-minipulation.js
@@ -2,3 +2,5 @@
 const user = [{name: 'Mr.Smit'}, {email: 'johnsmit@gmail.com'},{password:'ASDFG123456'}];
 const singleObject = user.reduce((i,j)=>({...i,...j}),{});
 console.log("Single Object Result>>",singleObject)
+const singleObjectSimpleMethod = Object.assign({},...user)
+console.log("Single Object Result>>",singleObjectSimpleMethod)
