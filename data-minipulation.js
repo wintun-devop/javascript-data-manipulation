496ddcc74ca646f7ddd8c9f88a9be9affd835707
Win Tun <wintun.hlaing@outlook.com>
2023-08-30 19:45:39 +0630

    Array_to_Single_Object

---
+++ data-minipulation.js
@@ -0,0 +1,4 @@
+/* Change to single object from an arry of having many object inside. */
+const user = [{name: 'Mr.Smit'}, {email: 'johnsmit@gmail.com'},{password:'ASDFG123456'}];
+const singleObject = user.reduce((i,j)=>({...i,...j}),{});
+console.log("Single Object Result>>",singleObject)
